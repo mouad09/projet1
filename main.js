f00555c1a298651ad2739a6f5ddd137b350bfa57
Mouad essaboni <mouadessabouni@gmail.com>
2023-06-04 13:30:02 +0100

    sauvegarde 1

---
+++ main.js
@@ -0,0 +1,140 @@
+
+
+
+
+
+
+
+//   function moyeen(){
+//      var nom = prompt('votre nom ');
+//      var pre = prompt('votre prenom ')
+//     var dateN = prompt('votre date de naissance : ')
+//      console.log("Nom condidat(e) : "+nom);
+//      console.log("Prenom condidat(e) : "+pre);
+//      console.log("Date de Naissance de  condidat(e) : "+dateN);
+//      var age = ( 2023 - dateN );
+//      console.log("Votre age est : "+age);
+//     var fr = parseInt(prompt('Note de francais'));
+//    var ar = parseInt(prompt('Note de arab'));
+//     var math = parseInt(prompt('Note de math'));
+//    console.log("Note de francais :"+fr);
+//   console.log("Note de arab :"+ar);
+//   console.log("Note de math :"+math);
+//    var resulta = (fr*2+ar+math*3)/6;
+//   console.log(resulta)
+//    if (resulta<20 , resulta>=17){
+//        console.log("parfait");
+//     }
+//     else if (resulta<17 , resulta>=15){
+//     console.log("tres bien")
+//    }
+//     else if (resulta<15 , resulta>=13) {
+//      console.log(" bien")
+//     }
+//    else if (resulta<13 , resulta>=10){
+//        console.log("passable")
+//    }
+//    else if (resulta<10 , resulta>=7){
+//        console.log("rattrapage")
+//   }
+//    else 
+//   console.log("no admi")
+//  }
+//  moyeen();
+//  console.log("moyen de ali est : "+moyeen());
+//  moyeen();
+//  console.log("moyen de hamza est : "+moyeen());
+//  moyeen();
+//  console.log("moyen de yassine est : "+moyeen());
+
+
+
+
+
+
+
+
+
+
+//   var TVA = prompt('votre salaire ')
+//   var res = TVA * 0.025;
+//  console.log("votre salaire est : "+TVA);
+//  console.log("VOTRE TVA "+res+" derham");
+//  console.log(typeof(TVA));
+//  console.log(typeof(resulta));
+//  console.log(typeof(dateN));
+
+
+//   function total(Qproduit,Pproduit){
+//       let total = Qproduit * Pproduit;
+//       return total;
+//   }
+//   alert("prix total est : "+total(14,12)+"Derhem");
+
+
+
+
+
+
+
+
+// function total(){
+//     let Qp = prompt('Quentite des produits : ')
+//     let Pp = prompt('Prix des produits : ')
+//     let res = Qp * Pp 
+//     console.log(res)
+
+// }
+// total(res)
+
+var nom = prompt("votre nom");
+var prenom = prompt("votre prenom");
+var DateN =  prompt("votre Date de naissance");
+var age = 2023 - DateN ;
+console.log("nom condidat(e) :"+nom);
+console.log("Prenom condidat(e) :"+prenom);
+console.log("Date de naissance de condidat(e): "+DateN);
+console.log("Age de condidat(e) : "+age);
+var pc = parseInt(prompt("Votre note phisique : "));
+var math = parseInt(prompt("Votre note math : "));
+var ar = parseInt(prompt("Votre note phisique : "));
+var res = (pc*3 + math*2 + ar)/6;
+console.log("votre note de phisique est : "+pc);
+console.log("votre note de math est : "+math);
+console.log("votre note de arabe est : "+ar);
+console.log("total des notes est : "+res);
+if (res<20 , res>=17){
+    console.log("parfait")
+} else if  (res<17 , res>=15){
+    console.log("tres bien")
+} else if (res<15 , res>=13){
+    console.log(" bien")
+}else if  (res<13 , res>=10){
+    console.log("passable")
+}else if (res<10 , res>=7){
+    console.log("rattrapage")
+}else if (res<7){
+    console.log('elimine')
+}
+var sal = prompt("votre salaire ")
+var res = sal * 0.025
+console.log("zakata dalk est : "+res+" dh");
+
+
+
+	
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
